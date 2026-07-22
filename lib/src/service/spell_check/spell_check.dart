71156afe558b94e7e86652c9680fe4a086a46299
liquidiert <haberederkorbinian@googlemail.com>
2026-07-22 16:57:43 +0200

    feat: extract spell checking service into separate package

--- lib/src/service/spell_check/spell_check.dart
+++ lib/src/service/spell_check/spell_check.dart
@@ -1,2 +1,12 @@
-export 'spell_check_configuration.dart';
-export 'spell_checker.dart';
+import 'package:hunspell_spell_check/hunspell_spell_check.dart';
+
+export 'package:hunspell_spell_check/hunspell_spell_check.dart'
+    show
+        HunspellSpellCheckConfiguration,
+        HunspellSpellCheckOptions,
+        HunspellSpellCheckService,
+        SpellChecker;
+
+/// Backwards-compatible alias for [HunspellSpellCheckOptions], which now
+/// lives in the extracted `hunspell_spell_check` package.
+typedef AppFlowySpellCheckConfiguration = HunspellSpellCheckOptions;
